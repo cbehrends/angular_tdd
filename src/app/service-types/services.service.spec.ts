af0354ea30b63b6fa79bf9139725f9673b461e20
coreybehrends <cbehrends@gmail.com>
2020-05-20 14:58:47 -0500

    Add missing tests for ServicesService

--- src/app/service-types/services.service.spec.ts
+++ src/app/service-types/services.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ServicesService } from './services.service';
+import {ServiceType} from './ServiceType';
 
 describe('ServicesService', () => {
   let service: ServicesService;
@@ -12,5 +12,15 @@ describe('ServicesService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add new Service description', () => {
+    service.addService(new ServiceType('FOO'));
+    expect(service.services.length).toBe(1);
+  });
+
+  it('should return a list of ServiceTypes', () => {
+    service.getServices();
+    expect(service.services.length).toBe(3);
   });
 });
